6bfb2a97f7b2b4097e7cd9ae9c0bf48f1e584092
Yuji Konishi <yuji.k64613@gmail.com>
2019-02-19 20:29:00 +0900

    v4

--- src/main/java/com/sample/Main.java
+++ src/main/java/com/sample/Main.java
@@ -59,7 +59,7 @@ public class Main {
             final String CONTENT_TYPE = "text/plane; charset=UTF-8";
             resp.setContentType(CONTENT_TYPE);
             final PrintWriter out = resp.getWriter();
-            out.println("hello, world! v3.0 devel");
+            out.println("hello, world! v4.0 devel");
             out.close();
         }
     }
